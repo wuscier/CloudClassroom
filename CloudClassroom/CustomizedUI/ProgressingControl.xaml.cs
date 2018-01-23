0a056a380252ab61b21bdaf8239b6ca55c718662
wuscier <1352288123@qq.com>
2018-01-23 13:49:34 +0800

    add progressing control

---
+++ CloudClassroom/CustomizedUI/ProgressingControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CloudClassroom.CustomizedUI
+{
+    /// <summary>
+    /// ProgressingView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ProgressingControl : Window
+    {
+        public ProgressingControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
