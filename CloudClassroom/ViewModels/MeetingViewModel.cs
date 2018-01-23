caf4c19fd83d0c9643c7752420021e11c9cee66a
wuscier <1352288123@qq.com>
2018-01-23 11:27:26 +0800

    meeting ui design

--- CloudClassroom/ViewModels/MeetingViewModel.cs
+++ CloudClassroom/ViewModels/MeetingViewModel.cs
@@ -1,6 +1,41 @@
-﻿namespace CloudClassroom.ViewModels
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Windows.Input;
+
+namespace CloudClassroom.ViewModels
 {
-    class MeetingViewModel
+    public class MeetingViewModel:BindableBase
     {
+        public MeetingViewModel()
+        {
+            InitData();
+        }
+
+        private void InitData()
+        {
+            WindowSizeChangedCommand = new DelegateCommand(() =>
+            {
+
+            });
+
+
+            WindowLocationChangedCommand = new DelegateCommand(() =>
+            {
+
+            });
+        }
+
+        public ICommand WindowSizeChangedCommand { get; set; }
+        public ICommand WindowLocationChangedCommand { get; set; }
+
+        public ICommand MicrophoneTriggerCommand { get; set; }
+        public ICommand AudioSettingsOpenedCommand { get; set; }
+        public ICommand AudioSelectedCommand { get; set; }
+        public ICommand SpeakerSelectedCommand { get; set; }
+        public ICommand CameraTriggerCommand { get; set; }
+        public ICommand VideoSettingsOpenedCommand { get; set; }
+        public ICommand VideoSelectedCommand { get; set; }
+        public ICommand OpenShareOptionsCommand { get; set; }
     }
 }
