b86e9ae804ed4cdcdd1afda00484454ffabdfb6d
wuscier <1352288123@qq.com>
2018-02-08 16:54:10 +0800

    add enum helper

--- CloudClassroom/Helpers/EnumHelper.cs
+++ CloudClassroom/Helpers/EnumHelper.cs
@@ -1,7 +1,27 @@
-﻿namespace CloudClassroom.Helpers
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace CloudClassroom.Helpers
 {
     public class EnumHelper
     {
+        public static string GetEnumDesc(Enum en)
+        {
+            Type type = en.GetType();
+            MemberInfo[] memberInfo = type.GetMember(en.ToString());
 
+            if (memberInfo != null && memberInfo.Length > 0)
+            {
+                object[] attributes = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attributes != null && attributes.Length > 0)
+                {
+                    return ((DescriptionAttribute)attributes[0]).Description;
+                }
+            }
+
+            return en.ToString();
+        }
     }
 }
