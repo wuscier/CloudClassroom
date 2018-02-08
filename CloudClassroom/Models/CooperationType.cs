1a003a620447e3bfe82240c43c0293d2cc0085dd
wuscier <1352288123@qq.com>
2018-02-08 17:15:51 +0800

    add cooperation type

---
+++ CloudClassroom/Models/CooperationType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace CloudClassroom.Models
+{
+    public enum CooperationType
+    {
+        [Description("校内")]
+        InnerSchool,
+        [Description("多校")]
+        InterSchool,
+    }
+}
