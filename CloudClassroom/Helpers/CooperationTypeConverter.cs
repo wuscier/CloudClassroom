3d0820a0fde9905a443c34e81ec3a00bc90eb7f5
wuscier <1352288123@qq.com>
2018-02-09 13:59:49 +0800

    add cooperation type conveter

--- CloudClassroom/Helpers/CooperationTypeConverter.cs
+++ CloudClassroom/Helpers/CooperationTypeConverter.cs
@@ -1,6 +1,26 @@
-﻿namespace CloudClassroom.Helpers
+﻿using CloudClassroom.Models;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CloudClassroom.Helpers
 {
-    class CooperationTypeConverter
+    public class CooperationTypeConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int intCooperationType = (int)value;
+
+            CooperationType cooperationType = (CooperationType)intCooperationType;
+
+            string cooperationName = EnumHelper.GetEnumDesc(cooperationType);
+
+            return cooperationName;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
